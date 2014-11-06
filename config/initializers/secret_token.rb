0aaa01b9bc9166a2e5d2c9ee9261f0e94c131ad7
Santu Mahapatra <santu.m@multunus.com>
2014-11-06 14:39:58 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoppingCart::Application.config.secret_key_base = '6b22b89159bca698c40f0ef0ec4826eb9e7105db153fb01d305475aff3ad80c7afff1b03880f725f495088fbaac2f24e539ff41fd72b591860bfb2c778c1c826'
